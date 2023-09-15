12f3774c932cee81100c1f8699d9ad9573886d88
Fridolin <39649581+VonFriedricht@users.noreply.github.com>
2023-09-15 08:51:27 +0200

    introducing tilemapper

---
+++ tools/TilesetMapper/script.js
@@ -0,0 +1,150 @@
+const tileMapping = Array(256).fill('grasserde_0_7.png');
+let currentCenterTileIndex = 0;
+
+function toggleTile(td) {
+    td.innerText = td.innerText === '1' ? '0' : '1';
+    updateCenterTile();
+}
+
+function updateCenterTile() {
+    setCurrentCenterTileIndex();
+    const centerImage = document.getElementById('centerImage');
+    centerImage.src = `./${tileMapping[currentCenterTileIndex]}`;
+}
+
+function setCurrentCenterTileIndex() {
+    const grid = document.querySelectorAll('#tileGrid td');
+    let binaryString = '';
+    const surroundingIndices = [0, 1, 2, 3, 5, 6, 7, 8];
+
+    surroundingIndices.forEach(index => {
+        binaryString += grid[index].innerText;
+    });
+
+    currentCenterTileIndex = parseInt(binaryString, 2);
+}
+
+const table = document.getElementById('imageGrid');
+for (let y = 0; y < 10; y++) {
+    const row = table.insertRow(y);
+    for (let x = 0; x < 10; x++) {
+        const cell = row.insertCell(x);
+        const img = document.createElement('img');
+        
+        img.src = `./grasserde_${y}_${x}.png`;
+        img.alt = `Grasserde Tile (${y}, ${x})`;
+        img.width = 50;  
+        img.height = 50; 
+        
+        img.addEventListener('click', () => {
+            tileMapping[currentCenterTileIndex] = `grasserde_${y}_${x}.png`;
+            updateCenterTile();
+        });
+
+        cell.appendChild(img);
+    }
+}
+
+function downloadTileMapping() {
+    const data = tileMapping.join('\n');
+    const blob = new Blob([data], {type: 'text/plain'});
+    const url = window.URL.createObjectURL(blob);
+    
+    const a = document.createElement('a');
+    a.style.display = 'none';
+    a.href = url;
+    a.download = 'tileMapping.txt';
+    
+    document.body.appendChild(a);
+    a.click();
+    
+    window.URL.revokeObjectURL(url);
+}
+
+
+// Initial setup: Set all surrounding tiles to 0 and update the center tile image
+document.querySelectorAll('#tileGrid td:not(#centerTile)').forEach(td => td.innerText = '0');
+updateCenterTile();
+document.getElementById('fileInput').addEventListener('change', loadTileMapping);
+
+function loadTileMapping() {
+    const fileInput = document.getElementById('fileInput');
+    const file = fileInput.files[0];
+
+    if (file) {
+        const reader = new FileReader();
+
+        reader.onload = function(event) {
+            const content = event.target.result;
+            const lines = content.split('\n');
+
+            // Die Zeilen aus der Datei werden in das tileMapping Array geladen.
+            for (let i = 0; i < lines.length && i < tileMapping.length; i++) {
+                tileMapping[i] = lines[i];
+            }
+
+            // Nachdem die Datei geladen wurde, aktualisieren Sie das centerTile.
+            updateCenterTile();
+        };
+
+        reader.readAsText(file);
+    } else {
+        alert('Bitte wählen Sie eine Datei aus.');
+    }
+}
+
+function listDefaultCombinations() {
+    const defaultTile = 'grasserde_0_7.png';
+    const defaultCombinations = [];
+
+    for (let i = 0; i < tileMapping.length; i++) {
+        if (tileMapping[i] === defaultTile) {
+            defaultCombinations.push(i.toString(2).padStart(8, '0')); 
+        }
+    }
+
+    const listElement = document.getElementById('defaultCombinationList');
+    listElement.innerHTML = ''; // Löschen Sie den bisherigen Inhalt
+
+    if (defaultCombinations.length === 0) {
+        listElement.textContent = 'Es gibt keine Kombinationen, die auf den Standardwert verweisen.';
+    } else {
+        listElement.textContent = 'Kombinationen, die auf den Standardwert verweisen:';
+        const ul = document.createElement('ul');
+        defaultCombinations.forEach(combination => {
+            const li = document.createElement('li');
+            li.textContent = combination;
+            ul.appendChild(li);
+        });
+        listElement.appendChild(ul);
+    }
+}
+
+function setRandomDefaultCenterTile() {
+    const defaultTile = 'grasserde_0_7.png';
+    const defaultIndices = [];
+
+    for (let i = 0; i < tileMapping.length; i++) {
+        if (tileMapping[i] === defaultTile) {
+            defaultIndices.push(i);
+        }
+    }
+
+    if (defaultIndices.length === 0) {
+        console.log('Es gibt keine Kombinationen, die auf den Standardwert verweisen.');
+        return;
+    }
+
+    // Einen zufälligen Index auswählen
+    const randomIndex = defaultIndices[Math.floor(Math.random() * defaultIndices.length)];
+    const binaryRepresentation = randomIndex.toString(2).padStart(8, '0');
+
+    // Das Grid entsprechend dem binären Wert aktualisieren
+    const cells = document.querySelectorAll('#tileGrid td:not(#centerTile)');
+    cells.forEach((cell, index) => {
+        cell.textContent = binaryRepresentation[index];
+    });
+
+    // Das centerTile aktualisieren
+    updateCenterTile();
+}
